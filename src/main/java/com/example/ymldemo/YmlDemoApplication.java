d82cf810fd81bdc3067939457f83651d64c118ac
jonx1183 <jonx1183@stud.kea.dk>
2023-02-08 11:06:30 +0100

    Yalm Test1

---
+++ src/main/java/com/example/ymldemo/YmlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ymldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmlDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YmlDemoApplication.class, args);
+  }
+
+}
